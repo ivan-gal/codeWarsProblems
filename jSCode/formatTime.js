67ef1e8815d4ec1caef1559ef88ad5ca8c8c3f59
Ivan Fernandez <ivan.ferndz94@gmail.com>
2021-03-10 14:52:16 +0100

    Buiding Time Formatter

--- jSCode/formatTime.js
+++ jSCode/formatTime.js
@@ -2,16 +2,11 @@
 
 function formatDuration(seconds) {
   let time = seconds;
-  const year = 24 * 365 * 60 * 60;
   const day = 24 * 60 * 60;
   const hour = 60 * 60;
   const min = 60;
-  const formatedTime = [0, 0, 0, 0, 0];
+  const formatedTime = [0, 0, 0, 0];
 
-  while (time > year) {
-    time /= year;
-    formatedTime[0] += 1;
-  }
   while (time > day) {
     time /= day;
     formatedTime[1] += 1;
@@ -26,11 +21,55 @@ function formatDuration(seconds) {
   }
 
   formatedTime[4] = time;
+  let finalInput = '';
 
-  const [totalYear, totalDay, TotalHour, TotalMin, TotalSec] = formatedTime;
+  let [totalDay, TotalHour, TotalMin, TotalSec] = formatedTime;
+  TotalSec = Math.round(TotalSec);
 
-  return `${totalYear} year ,${totalDay} day  ,${TotalHour} 
-  hour ${TotalMin} minutes ${Math.round(TotalSec)} seconds.`;
+  switch (totalDay) {
+    case 0:
+      totalDay = '';
+      break;
+    case 1:
+      totalDay = totalDay + ' day,';
+      break;
+    default:
+      totalDay = totalDay + ' days,';
+      break;
+  }
+  switch (TotalHour) {
+    case 0:
+      TotalHour = '';
+      break;
+    case 1:
+      TotalHour = TotalHour + ' hour,';
+      break;
+    default:
+      TotalHour = TotalHour + ' hours,';
+      break;
+  }
+  switch (TotalMin) {
+    case 0:
+      TotalMin = '';
+      break;
+    case 1:
+      TotalMin = TotalMin + ' minute';
+      break;
+    default:
+      TotalMin = TotalMin + 'minutes';
+      break;
+  }
+
+  switch (TotalSec) {
+    case 1:
+      TotalSec = TotalSec + ' second';
+      break;
+    default:
+      TotalSec = TotalSec + ' seconds';
+      break;
+  }
+
+  return `${totalYear}${totalDay}${TotalHour}${TotalMin} and ${TotalSec}`;
 }
 
 console.log(formatDuration(210102302032332301301));
