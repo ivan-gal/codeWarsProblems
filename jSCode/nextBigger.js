d127836778ee438a41a8ed1dafdb698e508f1e18
Ivan Fernandez <ivan.ferndz94@gmail.com>
2021-03-12 04:18:45 +0100

    Find the next bigger number Algo

--- jSCode/nextBigger.js
+++ jSCode/nextBigger.js
@@ -1,21 +1,32 @@
 'use strict';
 function nextBigger(n) {
+  //Primero convertimos el numero a array para poder manejarlo mejor.
   const numberToArray = n.toString().split('');
   let lowestNumber = 100;
   let lowestPosition = 0;
+
+  //Recorremos el array de derecha a izquierda
   for (let i = numberToArray.length - 1; i >= 0; i--) {
+    //para encontrar el primer número que sea mayor al siguiente.
     if (numberToArray[i] > numberToArray[i - 1]) {
-      for (let indexI = i - 1; indexI < numberToArray.length; indexI++) {
-        console.log(indexI);
-        if (numberToArray[indexI] < lowestNumber) {
-          console.log(lowestNumber);
+      //Despues recorremos el array de izquierda a derecha para intercambiar el numero i - 1, con el menor numero de la izquierda
+      for (let indexI = i; indexI < numberToArray.length; indexI++) {
+        if (numberToArray[indexI] > numberToArray[i - 1]) {
           lowestNumber = numberToArray[indexI];
           lowestPosition = indexI;
         }
       }
-      const tmp = numberToArray[i];
-      numberToArray[i] = numberToArray[lowestPosition];
-      numberToArray[lowestPosition] = tmp;
+      //Despues de esto, intercambiamos los numéros.
+      const tmp = numberToArray[lowestPosition];
+      numberToArray[lowestPosition] = numberToArray[i - 1];
+      numberToArray[i - 1] = tmp;
+
+      //Y ordenamos la par
+      let RightPart = numberToArray.splice(i).sort();
+      for (let value of RightPart) {
+        numberToArray.push(value);
+      }
+
       break;
     }
   }
@@ -23,4 +34,4 @@ function nextBigger(n) {
   if (finalNumber === n) return -1;
   return finalNumber;
 }
-console.log(nextBigger(42321));
+console.log(nextBigger(629286));
