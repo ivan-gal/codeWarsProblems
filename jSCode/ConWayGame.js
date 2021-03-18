095b7a3b467cbc5b75b2aca4ecc38514257f2624
Ivan Fernandez <ivan.ferndz94@gmail.com>
2021-03-18 21:31:39 +0100

    Fixed comments

--- jSCode/ConWayGame.js
+++ jSCode/ConWayGame.js
@@ -17,7 +17,7 @@ const gliders = [
 //The rules of the game are:
 
 //Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-//Any live cell with more than three live neighbours dies, as if by overcrowding.
+//Any live cell with more than three live neighbours dies,  by overcrowding.
 //Any live cell with two or three live neighbours lives on to the next generation.
 //Any dead cell with exactly three live neighbours becomes a live cell.
 
